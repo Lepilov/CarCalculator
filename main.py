d38eb2f9128474059027dbda649abea311a27f1a
Lepilov <lepilov18@gmail.com>
2024-01-21 13:34:28 +0300

    Мини-проект 2

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
 
     calc.add_car(calculator.Car(name="Toyota", price=30000, fuel_economy=7, service_cost=1200, insurance_cost=2500))
 
+    calc.add_car(calculator.Car(name="BMW", price=30000, fuel_economy=7, service_cost=1200, insurance_cost=2500))
+
     calc.add_car(calculator.ElectricCar(name="Tesla", price=200000, insurance_cost=55000, power_consumption=150))
 
     calc.print_cars()
